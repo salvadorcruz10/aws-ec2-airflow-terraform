7ff0dc27433c49acdc1d58cfd206c9bca6bb6f23
salvador.cruz <salvador.cruz@wizeline.com>
2020-01-28 16:22:23 -0600

    Add Infrastructure as Code
    Signed-off-by: salvador.cruz <salvador.cruz@wizeline.com>

---
+++ IaC/s3.tf
@@ -0,0 +1,14 @@
+
+# -------------------------------------------
+# CREATE A S3 BUCKET TO STORAGE AIRFLOW LOGS
+# -------------------------------------------
+
+resource "aws_s3_bucket" "airflow_logs" {
+  bucket        = var.s3_bucket_name
+  acl           = "private"
+  force_destroy = true
+
+  tags = module.airflow_labels.tags
+}
+
+
